cbd098e94e2ea1cfce7dd8b6001174ce7b580ea5
Lukas <lukas.nemec.cz@gmail.com>
2018-10-02 18:38:38 +0200

    Added Rocket and rocket controls

---
+++ Assets/Rocket/Rocket.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Thrust();
+        Rotate();
+	}
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true;
+
+        float rotationFrameSpeed = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationFrameSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationFrameSpeed);
+        }
+
+        rigidbody.freezeRotation = false;
+    }
+}
